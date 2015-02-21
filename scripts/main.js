cadce734f2e6d2f40ea1667c1ea0a74208eae454
McLeanBH <mcleanbh@gmail.com>
2015-02-20 20:52:05 -0500

    menu rendered

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,11 @@
 (function(){
   'use strict';
-
-//------------------------
-// MODELS & COLLECTIONS //
-//------------------------
-
+  
   var Menu = Backbone.Model.extend({
     idAttribute: 'objectId',
-    defaults: function(attributes){
-      attributes = attributes || {};
-      return _.defaults(attributes, {
+    defaults: function(opts){
+      opts = opts || {};
+      return _.defaults({
         item_title: 'default',
         item_description: 'default',
         item_price: 'default',
@@ -24,45 +20,57 @@
     parse: function(food) { return food.results; }
   });
 
-//------------------------
-// VIEWS //
-//------------------------
-
   var MenuListView = Backbone.View.extend({
-    el: '.sidebar',
+    el: '.menu',
     template: _.template($('[data-template-name="post-li"]').text()),
 
 
     events: {
-      'click .post-li': 'showFullPost'
+      'click .post-li': 'showMenu'
     },
 
-    showFullPost: function() {
+    showMenu: function() {
     },
 
     render: function() {
       var that = this;
       this.collection.each(function(menu) {
-        that.$el.append( that.template( post.toJSON() ) );
+        that.$el.append( that.template( menu.toJSON() ) );
       });
       return this;
     }
  });
 
+ var MenuNewView = Backbone.View.extend({
+   el: '#item-info',
+   events: {
+     'submit' : 'itemInfo'
+   },
+   itemInfo: function(e) {
+     e.preventDefault();
+     var itemTitle = this.$('.itemTitleInput').val();
+     var itemDescription = this.$('.itemDescriptionInput').val();
+     var itemPrice = this.$('.itemPriceInput').val();
+     var itemCategory = this.$('.itemCategoryInput').val();
 
- var PostFullView = Backbone.View.extend({
+     this.collection.create({item_title: itemTitle, item_description: itemDescription, item_price: itemPrice, item_category: itemCategory});
+     this.$('.itemTitleInput').val('');
+     this.$('.itemDescriptionInput').val('');
+     this.$('.itemPriceInput').val('');
+     this.$('.itemCategoryInput').val('');
+   }
+ });
+
+
+ var MenuFullView = Backbone.View.extend({
    tagName: 'div',
-   className: 'showPost',
-   template: _.template($('[data-template-name="showPost"]').text()),
+   className: 'showMenu',
+   template: _.template($('[data-template-name="showMenu"]').text()),
    render: function() {
      this.$el.append(this.template(this.model.toJSON()));
      return this;
    }
  });
-
-//------------------------
-// ROUTER //
-//------------------------
 
 var AppRouter = Backbone.Router.extend ({
   routes: {
@@ -81,33 +89,14 @@ var AppRouter = Backbone.Router.extend ({
       that.menusList.render();
     });
   },
-
-
-  showMenu: function(id){
-    var that = this;
-    this.menus.fetch().done(function() {
-      foundModel = that.menus.get(id);
-      var menuFull = new MenuFullView({model: foundModel});
-      menuFull.render();
-      $('.full-post').html(menuFull.el);
-    });
-  }
 });
-
-//------------------------
-// CONFIGURATION //
-//------------------------
 
 $.ajaxSetup ({
   headers: {
-    "X-Parse-Application-Id": "YIAK0okVUTwf5uylx6nxDATgoA5mdAclhUfycbRe",
-    "X-Parse-REST-API-Key": "3JVELiFs5rCa9TXmmPHE8jQrWKG4Sg2440UYxoMw"
+    "X-Parse-Application-Id": "3sSDwBGBkqjPYHuD6sv7RlG7B0hUPaDgqJzFVAmb",
+    "X-Parse-REST-API-Key": "KGBI3JFmgSKKe0Z1SCU2X50OiuRGZ74xmMUzteqT"
   }
 });
-
-//------------------------
-// GLUE CODE //
-//------------------------
 
   $(document).ready(function(){
     window.router = new AppRouter();
@@ -115,30 +104,3 @@ $.ajaxSetup ({
   });
 
 })();
-
-
-
-// //------------------------
-// // VIEWS //
-// //------------------------
-//
-//   var MenuListView = Backbone.View.extend({
-//     el: '#item-info',
-//     events: {
-//       'submit': 'itemInfo'
-//     },
-//     itemInfo: function(e){
-//       e.preventDefault();
-//       var itemTitle = this.$('.itemTitleInput').val();
-//       var itemDescription = this.$('.itemDescriptionInput').val();
-//       var itemPrice = this.$('.itemPriceInput').val();
-//       var itemType = this.$('.itemTypeInput').val();
-//
-//       this.collection.create({item_title: itemTitle, item_description: itemDescription, item_price: itemPrice, item_type: itemType});
-//       this.$('.itemTitleInput').val('');
-//       this.$('.itemDescriptionInput').val('');
-//       this.$('.itemPriceInput').val('');
-//       this.$('.itemTypeInput').val('');
-//     }
-//   });
-//
